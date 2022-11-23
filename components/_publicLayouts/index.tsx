f5959957577fc07a7dba2fdbf38872211e07a20c
kangmiftah <miftahudin903@gmail.com>
2022-11-24 00:46:22 +0700

    update useLayout fix ssg

--- components/_publicLayouts/index.tsx
+++ components/_publicLayouts/index.tsx
@@ -1,12 +1,17 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Nav from "./nav/nav";
 
 export default function Layout<FC>(page: React.ReactElement) {
+   const [showNav, setShowNav] = useState<boolean>(false);
+   useEffect(() => {
+      setShowNav(true);
+    }, []);
+
    return (
       <>
          <div className=" min-h-full max-h-full w-height overflow-hidden">
             <div className=" overflow-auto max-h-full ">
-               <Nav />
+               { showNav &&  <Nav />}
                <div className="container mx-auto px-4 py-4 h-[calc(100vh*2)]">
                   {page}
                </div>
