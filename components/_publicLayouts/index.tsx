eafd2f7e757910d54f3142f8d6af43139ddb37c8
kangmiftah <miftahudin903@gmail.com>
2022-11-25 23:46:33 +0700

    update fungsi carousel

--- components/_publicLayouts/index.tsx
+++ components/_publicLayouts/index.tsx
@@ -13,10 +13,10 @@ export default function Layout<FC>(page: React.ReactElement) {
 
    return (
       <>
-         <div className=" min-h-full max-h-full w-height overflow-hidden">
-            <div className=" overflow-auto max-h-full ">
+         <div className=" min-h-full w-height">
+            <div className="  h-full ">
                { showNav &&  <Nav />}
-               <div className={`container mx-auto px-4 py-4 h-[calc(100vh*2)] ${layouState.isSticky ? "mt-[100px]" : ""}`}>
+               <div className={`container mx-auto px-4 py-4 min-h-screen ${layouState.isSticky ? "mt-[100px]" : ""}`}>
                   {page}
                </div>
             </div>
