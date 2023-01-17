c29acf9c7ce15ea6b3c97722608e87573f11f6a8
kangmiftah <miftahudin903@gmail.com>
2023-01-17 15:29:52 +0700

    update perbaikan management action

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,29 +1,48 @@
 import { GetServerSideProps } from "next";
 import { ReactElement } from "react"
-import { AdminLayout } from "../../../components";
+import { AdminLayout, Card } from "../../../components";
 import authAdminMiddleware from "../../../_modules/midleware/authAdminMiddleware";
-
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
 const Page = (props: any) => {
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
   return (
     <>
-    <div className="grid grid-cols-3" >
-      <div  className="">
-        <h1 className="text-3xl font-bold underline">
-        Ini adalah admin yang bertanggung jawab mengelola applikasi 
-        </h1>
-      </div>
-      <div  className="">
-        <h1 className="text-3xl font-bold underline">
-        Ini adalah admin yang bertanggung jawab mengelola applikasi 
-        </h1>
-      </div>
-      <div  className="">
-        <h1 className="text-3xl font-bold underline">
-        Ini adalah admin yang bertanggung jawab mengelola applikasi 
-        </h1>
-      </div>
-    </div>
+        <div className="mt-3 grid grid-cols-3 gap-2">
+          <div>
+            <Card>
+              <Card.Header>
+                <h1 className="text-xl"> Chart 1 </h1>
+              </Card.Header>
+              <Card.Body>
+              <Doughnut data={{
+                  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                  datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1,
+                    backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                    ],
+                  }]
+              }} />
+              </Card.Body>
+            </Card>
+          </div>
+          <div>
+            
+          </div>
+          <div>
+            
+          </div>
+        </div>
     </>
   )
 }
