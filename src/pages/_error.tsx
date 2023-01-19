7b12e4de6d3615dcd817dd7f2e3eb7cef67696d7
kangmiftah <miftahudin903@gmail.com>
2023-01-20 00:12:31 +0700

    update error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -4,7 +4,12 @@ export default function(){
 
 
 
-   return<div className=" flex w-full h-screen justify-center items-center align-middle" >
-      <h1 className="font-bold text-2xl text-danger"> Not Found</h1>
+   return<div className=" flex flex-2 w-full h-screen justify-center items-center align-middle" >
+      <div className="text-center gap-0">
+         
+         <span className="text-[90pt] m-0">404 </span>
+         <hr/>
+         <span className="font-bold text-2xl text-danger m-0"> Not Found</span>
+      </div>
    </div>
 }
